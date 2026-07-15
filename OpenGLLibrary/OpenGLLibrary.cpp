2fa10d26b058279e024996b57f4d7aceccfcf3ae
Marco Fallace <fallace.marco@gmail.com>
2026-07-15 13:02:22 +0200

    Add project files.

---
+++ OpenGLLibrary/OpenGLLibrary.cpp
@@ -0,0 +1,12 @@
+﻿// OpenGLLibrary.cpp : Defines the entry point for the application.
+//
+
+#include "OpenGLLibrary.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
